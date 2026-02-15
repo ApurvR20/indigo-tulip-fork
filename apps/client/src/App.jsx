c48cb14f120e74574e1d0a24d259cc979aef522f
blueotino <noegl3609@gmail.com>
2026-02-15 21:46:07 +0100

    9 create loadingspinner component (#22)
    * Save local .gitignore
    
    * loading spinner initial

--- apps/client/src/App.jsx
+++ apps/client/src/App.jsx
@@ -1,7 +1,10 @@
+ 
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SignUpPage from './pages/Sign-upPage';
 
 import LoginPage from './pages/LoginPage';
+
 
 function App() {
 
